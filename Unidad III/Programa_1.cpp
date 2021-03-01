8f2426f4128244867cd8b4c40a47949401455208
Williams R. Villavizar Hdez <73804378+willrd14@users.noreply.github.com>
2021-03-01 04:42:17 -0800

    Unidad III

---
+++ Unidad III/Programa_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string User;
+	int cantMayusculas = 0;
+	int cantMinusculas = 0;
+	int cantDigitos = 0;
+	int cantSigPunt = 0;
+	int cantAlfNu = 0;
+	
+	cout << "Introduzca la palabra deceada:\n";
+	cin >> User;
+	
+	system("pause");
+	return 0;
+}
