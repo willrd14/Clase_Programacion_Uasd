8a51bf9b6dccd61907a26fb62ead6b1bbf2ad179
williams <williamsgamer02@gmail.com>
2022-03-19 11:59:32 -0400

    laura

--- Introduccion_a_la_Programacion/Lab/peso.cpp
+++ Introduccion_a_la_Programacion/Lab/peso.cpp
@@ -7,13 +7,14 @@ struct TMB
 {
     float mujer;
     float hombre;
+    float nivelActFis;
 };
 
 struct Datos
 {
     string nombre;
     int edad;
-    char sexo[5];
+    char sexo[15];
     float peso;
     float estatura;
     char nivelActFis[5];
@@ -30,8 +31,11 @@ int main()
     cout << "Digite su edad: ";
     cin >> dato.edad;
 
-    cout << "Digite su sexo [M] o [F]: ";
+    cout << "Digite su sexo [masculino] o [femenino]: ";
     cin >> dato.sexo;
+
+    cout << "Digite su estatura: ";
+    cin >> dato.estatura;
 
     cout << "Digite su peso: ";
     cin >> dato.peso;
@@ -39,12 +43,19 @@ int main()
     cout << "Nivel de actividad fisica Nula[N] Leve[L] Moderada[M] Regula[R] Extrema[E]: ";
     cin >> dato.nivelActFis;
 
+    if (dato.sexo == femenino)
+    {
+        tmb.mujer = (10 * dato.peso) + (6.25 * dato.estatura) - (5 * dato.edad) - 161;
+        cout << tmb.mujer << endl;
+    }
+
+    else
+    {
+        tmb.hombre = (10 * dato.peso) + (6.25 * dato.estatura) - (5 * dato.edad) + 5;
+        cout << tmb.hombre << endl;
+    }
+
     
-
-
-
-
-
 
     return 0;
 }
