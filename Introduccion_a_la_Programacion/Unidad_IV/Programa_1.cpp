2ad66e7147951ea8e9b6187f71bbd42d18087d1b
Williams <williamsgamer02@gmail.com>
2022-03-30 10:53:28 -0400

    Programa_1

--- Introduccion_a_la_Programacion/Unidad_IV/Programa_1.cpp
+++ Introduccion_a_la_Programacion/Unidad_IV/Programa_1.cpp
@@ -1,48 +1,76 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
-struct ventaBebida
-{
-    string nombreBebida;
-    double precioBebida;
-    int cantidadEnMaquina;
-    float botellaAgua = 20.00;
-    float cocaCola = 25.00;
-    float sevenUp = 25.00;
-    float montainDew = 30.00;
-    float jugoV8 = 50.00;
+struct StBebida {
+	string nombre;
+	double precio;
+	int    cantidad;
 };
 
 int main()
 {
-    ventaBebida venta;
+  const int SALIR = 9;
+  int    opcion;
+  double montoIntroducido;
+  double montoVentaTotal(0);
 
-    while (1)
-    {
-        cout << "--------------------| Bienvenido |-------------------" << endl;
-        cout << "-----------------------------------------------------" << endl;
-        cout << "Nombre de la Bebida |  Costo  |  Cantidad en Maquina " << endl;
-        cout << "-----------------------------------------------------" << endl;
-        cout << "1-Botella de Agua      20.00              20" << endl;
-        cout << "2-Coca Cola            25.00              20" << endl;
-        cout << "3-Seven Up             25.00              20" << endl;
-        cout << "4-Montain Dew          30.00              20" << endl;
-        cout << "5-Jugo V8              50.00              20" << endl;
-        cout << "0-Salir" << endl;
+	vector<StBebida> bebida;
+  bebida.push_back({"Botella de Agua",20.00, 20});
+  bebida.push_back({"Coca Cola",      25.00, 20});
+  bebida.push_back({"Seven Up",       25.00, 20});
+  bebida.push_back({"Montain Dew",    30.00, 20});
+  bebida.push_back({"Jugo V8",        50.00, 20});
 
-        cout << "Que bebida va a decear: ";
-        getline (cin, venta.nombreBebida);
+  while( true )
+  {
+        //- Mostrando las bebidas disponibles
+        cout << "Bebidas Disponibles" << '\n'
+             << "-------------------" << '\n';
+        for( int ind = 0; ind < bebida.size(); ++ind )
+           cout << ind  + 1 << ".)"
+                << setw(20) << bebida.at(ind).nombre   << '\t'
+                << bebida.at(ind).precio   << '\t'
+                << bebida.at(ind).cantidad << '\t'
+                << endl;
 
-        cout << "Cuantas va a decear: ";
-        cin >> venta.cantidadEnMaquina;
-    }
-    
-    
+        do {
+            cout << "Introducir Num. Opcion (9-Terminar) : ";
+            cin  >> opcion;
+        } while ( ( opcion < 1 || opcion > 5 ) && opcion != SALIR );
 
+        if ( opcion == SALIR ) break;
 
+        if ( bebida.at(opcion - 1).cantidad < 1 )
+        {
+             cout << "\n\n"
+                  << "Vevida Agotada !!!!!"
+                  << "\n\n"
+                  << endl;
+             continue;
+        }
 
+        do {
+            cout << "Introducir Monto (Mayor o igual que $" << bebida.at(opcion - 1).precio << ") : ";
+            cin  >> montoIntroducido;
+        } while ( montoIntroducido < bebida.at(opcion - 1).precio );
 
-    return 0;
+        double cambio = montoIntroducido - bebida.at(opcion - 1).precio;
+        --bebida.at(opcion - 1).cantidad;
+
+        cout << "\n\nCambio : " << cambio << endl;
+  }
+
+  montoVentaTotal = 0;
+
+  for( int ind = 0; ind < bebida.size(); ++ind ) {
+      montoVentaTotal += bebida.at(ind).precio * ( 20 - bebida.at(ind).cantidad );
+  }
+
+  cout << "Venta Total : " << montoVentaTotal << endl;
+
+  return 0;
 }
