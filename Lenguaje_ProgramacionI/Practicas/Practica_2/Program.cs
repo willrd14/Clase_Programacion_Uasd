451f8014982728ae620ba76502a69c129a031da5
williams <williamsgamer02@gmail.com>
2022-09-01 09:35:07 -0400

    Agregado de clase pasada

--- Lenguaje_ProgramacionI/Practicas/Practica_2/Program.cs
+++ Lenguaje_ProgramacionI/Practicas/Practica_2/Program.cs
@@ -1,54 +1,51 @@
-﻿//Implementacion de acciones diferente en calculadora.
+﻿class Calculadora{
 
-Console.WriteLine("Que operacion desea hacer?");
-Console.WriteLine("1. Suma");
-Console.WriteLine("2. Resta");
-Console.WriteLine("3. Multiplicacion");
-Console.WriteLine("4. Division");
-Console.WriteLine("");
-int op = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(""); 
+    static void Main(string[] args)
+    { 
+        double num1=0;
+        double num2=0;
+        string entrada;
+        char op='\u0000';
+        double resultado=0;
+        if (args.Length == 0){
+                Console.Clear();
+                Console.WriteLine("Digite Valor1: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite Valor2: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+            
+                Console.WriteLine("Que operacion desea hacer?");
+                Console.WriteLine("[ +, -, /, * ]");
+                entrada=(Console.ReadLine());        
+         } else if (args.Length == 3){
+                num1 = Convert.ToDouble(args[0]);
+                num2 = Convert.ToDouble(args[2]);
+                string str1=args[1];
+                op=str1[0];            
+                   
+         }
+        
+         resultado=Calculadora.Calcular(num1, num2,op);        
+         Console.WriteLine(resultado);
+     }
 
-if (op == 1)
-{
-    Console.WriteLine("Digite Valor deseado: ");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Digite Valor deseado: ");
-    int num2 = Convert.ToInt32(Console.ReadLine());
+     static double Calcular(double a, double b, char op){
 
-    int suma = num1 + num2;
-    Console.WriteLine("El resultado de la suma es: " + suma);
-}
+        double result = 0;
+        switch(op){ 
+            case '+': result =a+b;
+             break;
+             case '-': result =a-b;
+             break;
+             case '/': result =a/b;
+             break;
+             case '*': result =a*b;
+             break;                          
+        }
 
-if (op == 2)
-{
-    Console.WriteLine("Digite Valor deseado: ");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Digite Valor deseado: ");
-    int num2 = Convert.ToInt32(Console.ReadLine());
 
-    int resta = num1 - num2;
-    Console.WriteLine("El resultado de la resta es: " + resta);
-}
+        return result;
 
-if (op == 3)
-{
-    Console.WriteLine("Digite Valor deseado: ");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Digite Valor deseado: ");
-    int num2 = Convert.ToInt32(Console.ReadLine());
+     }   
 
-    int mult = num1 * num2;
-    Console.WriteLine("El resultado de la multiplicacion es: " + mult);
-}
-
-if (op == 4)
-{
-    Console.WriteLine("Digite Valor deseado: ");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Digite Valor deseado: ");
-    int num2 = Convert.ToInt32(Console.ReadLine());
-
-    int div = num1 / num2;
-    Console.WriteLine("El resultado de la divicion es: " + div);
 }
