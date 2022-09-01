6503ff62b62fc38a5a9cbf029ac5f9700ce6f4f9
williams <williamsgamer02@gmail.com>
2022-09-01 09:29:10 -0400

    Clase

--- Lenguaje_ProgramacionI/Practicas/Practica_2/Program.cs
+++ Lenguaje_ProgramacionI/Practicas/Practica_2/Program.cs
@@ -1,15 +1,20 @@
 ﻿//Implementacion de acciones diferente en calculadora.
 
-Console.WriteLine("Digite Valor deseado: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Digite operacion deseada (+,-,*,/)");
-char op = Console.ReadLine();
-Console.WriteLine("Digite Valor deseado: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Que operacion desea hacer?");
+Console.WriteLine("1. Suma");
+Console.WriteLine("2. Resta");
+Console.WriteLine("3. Multiplicacion");
+Console.WriteLine("4. Division");
+Console.WriteLine("");
+int op = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(""); 
 
 if (op == 1)
 {
-    
+    Console.WriteLine("Digite Valor deseado: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite Valor deseado: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
 
     int suma = num1 + num2;
     Console.WriteLine("El resultado de la suma es: " + suma);
