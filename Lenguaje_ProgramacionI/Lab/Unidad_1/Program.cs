ffb62a668f107f255c553be8a581953fcb047537
williams <williamsgamer02@gmail.com>
2022-09-07 22:04:02 -0400

    Clase 06/09/2022

---
+++ Lenguaje_ProgramacionI/Lab/Unidad_1/Program.cs
@@ -0,0 +1,17 @@
+﻿class buroCredito{
+    private float libreCosto;
+    private float dataCredito;
+    private float trans_Union;
+    private bool lider;
+    private string leyHabeasData;
+
+    public void acceder(){
+
+    } 
+    public void permitir(){
+
+    }
+    public void accesoGratuito(){
+        
+    }
+}
