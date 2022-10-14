baaaa01428d6992cffbe0c29bd294cc65cd96bd7
Williams R. Villavizar Hdez <73804378+willrd14@users.noreply.github.com>
2022-10-13 23:18:48 -0400

    Unidad 3

---
+++ Lenguaje_ProgramacionI/Unidad_3/ejercicio2/VacationScaleTwoTest.cs
@@ -0,0 +1,13 @@
+using System;
+public class VacationScaleTwoTest {
+   
+ static void Main (String[] args) {
+ 
+    VacationScaleTwo myVacationScaleTwo = new VacationScaleTwo();
+  
+    myVacationScaleTwo.setVacationScale();
+
+    myVacationScaleTwo.displayVacationDays();
+
+  } 
+}
