c3687701b0286924438deeab330e5d6ae83a277e
Williams R. Villavizar Hdez. <73804378+willrd14@users.noreply.github.com>
2023-11-25 15:53:28 -0400

    11/25/23

--- Lenguaje_ProgramacionI/proyectoFinal/Program.cs
+++ Lenguaje_ProgramacionI/proyectoFinal/Program.cs
@@ -16,6 +16,26 @@ namespace ProyectoFinal{
         Console.WriteLine("2. Usuario");
         Console.WriteLine("3. Salir");
         option = int.Parse(Console.ReadLine());
+
+        switch (option){
+
+            case 1:
+            bibliotecario();
+            break;
+
+            case 2:
+            usuario();
+            break;
+
+            caso 3:
+            Console.WriteLine("Hasta luego!!");
+            break;
+
+            default:
+            Console.WriteLine("Opcion no valida, intentelo de nuevo.");
+            break;
+
+        }
     }
 
     class Bibliotecario : Sistema {
@@ -23,7 +43,7 @@ namespace ProyectoFinal{
         public string admin = "Admin";
         public int adminClave = 1234;
 
-        public override bibliotecario{
+        public void  bibliotecario(){
             Console.WriteLine("Digite su usuario: ");
             admin = Console.ReadLine();
 
@@ -36,10 +56,12 @@ namespace ProyectoFinal{
                 Console.WriteLine("Usuario y/o clave invalida");
             }
         }
+
+
         
     }
 
-    class Usuario : Sistema {
+    class Usuario : Sistema, Articulo {
         
         public string nombre;
         public string apellido;
@@ -49,36 +71,115 @@ namespace ProyectoFinal{
         public int clave;
         public int obtion;
 
-        Console.WriteLine("Usted esta registrado? : ");
-        Console.WriteLine("1. Si");
-        Console.WriteLine("2. No");
-        obtion = int.Parse(Console.ReadLine());
+        string libro = @"C:\Users\willi\Code\Clase_Programacion_Uasd\Lenguaje_ProgramacionI\proyectoFinal\Libros.txt";
+        string contenido = File.ReadAllText(libro);
 
-        switch (obtion){
-            case 1:
-            Console.WriteLine("Ingrese el Usuario: ");
-            usuario = Console.ReadLine();
-            Console.WriteLine("Ingrese la Clave: ");
-            clave = int.Parse(Console.ReadLine());
+        public override usuario(){
+            Console.WriteLine("Usted esta registrado? : ");
+            Console.WriteLine("1. Si");
+            Console.WriteLine("2. No");
+            obtion = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Bienvenido de nuevo!!");
-            break;
+            switch (obtion){
+                case 1:
+                Console.WriteLine("Ingrese el Usuario: ");
+                usuario = Console.ReadLine();
+                Console.WriteLine("Ingrese la Clave: ");
+                clave = int.Parse(Console.ReadLine());
 
-            case 2:
-            Console.WriteLine("Ingrese su nombre: ");
-            nombre = Console.ReadLine();
-            Console.WriteLine("Ingrese su apellido: ");
-            apellido = Console.ReadLine();
-            Console.WriteLine("Ingrese su direccion: ");
-            direccion = Console.ReadLine();
-            Console.WriteLine("Ingrese su telefono: ");
-            telefono = int.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese su usuario: ");
-            usuario = Console.ReadLine();
-            Console.WriteLine("Ingrese su clave: ");
-            clave = Console.ReadLine();
-            Console.WriteLine("Bienvenido!! y gracias por registrarse.");
-            break;
+                Console.WriteLine("Bienvenido de nuevo!!");
+                break;
+
+                case 2:
+                Console.WriteLine("Ingrese su nombre: ");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese su apellido: ");
+                apellido = Console.ReadLine();
+                Console.WriteLine("Ingrese su direccion: ");
+                direccion = Console.ReadLine();
+                Console.WriteLine("Ingrese su telefono: ");
+                telefono = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese su usuario: ");
+                usuario = Console.ReadLine();
+                Console.WriteLine("Ingrese su clave: ");
+                clave = Console.ReadLine();
+                Console.WriteLine("Bienvenido!! y gracias por registrarse.");
+                break;
+            }
+        } 
+
+        public void pedir(){
+            Console.WriteLine("Que articulo deseas pedir: ")
+            Console.WriteLine("1. Libro");
+            Console.WriteLine("2. VideoJuego");
+            Console.WriteLine("3. Consola");
+            Console.WriteLine("4. Pelicula");
+            Console.WriteLine("5. Salir");
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch(opcion){
+
+                case 1:
+                Console.WriteLine("Desea ver cuales libros tenemos disponibles?");
+                Console.WriteLine("1. Si");
+                Console.WriteLine("2. No");
+                int opcionaLibro = int.Parse(Console.ReadLine());
+                switch(opcionaLibro){
+                    case 1:
+                    Console.WriteLine(contenido);
+                    Console.WriteLine("5. Salir");
+                    int opcion2 = int.Parse(Console.ReadLine());
+                    break;
+
+                    case 2:
+                    Console.WriteLine("Volviendo al menu.....");
+                    pedir();
+                    break;
+                    
+                }
+
+                case 2:
+                Console.WriteLine("Desea ver cuales videojuegos tenemos disponibles?");
+                Console.WriteLine("1. Si");
+                Console.WriteLine("2. No");
+                int opcionesVideojuego = int.Parse(Console.ReadLine());
+                break;
+
+                case 3:
+                Console.WriteLine("Desea ver cuales consolas tenemos disponibles?");
+                Console.WriteLine("1. Si");
+                Console.WriteLine("2. No");
+                int opcionesConsola = int.Parse(Console.ReadLine());
+                break;
+
+                case 4:
+                Console.WriteLine("Desea ver cuales peliculas tenemos disponibles?");
+                Console.WriteLine("1. Si");
+                Console.WriteLine("2. No");
+                int opcionesPelicula = int.Parse(Console.ReadLine());
+                break;
+
+                case 5:
+                Console.WriteLine("Desea ver cuales peliculas tenemos disponibles");
+                Console.WriteLine("1. Si");
+                Console.WriteLine("2. No");
+                int opcionesPeliculas = int.Parse(Console.ReadLine());
+                break;
+
+                default:
+                Console.WriteLine("Opcion no valida");
+                break;
+
+            }
         }
+    }
+
+    class Articulo : Usuario, Bibliotecario{
+        
+        public bool articulos_disponibles;
+        public string registrar;
+        public string agregar;
+        public int actualizar;
+        public bool vencido;
     }
 }
