f041665f22c5ff3e6bb4ebdba9b428efb0375b0a
Williams <williamsgamer02@gmail.com>
2024-02-21 23:07:12 -0400

    clase

--- Lenguaje_ProgramacionI/Laboratorio/tarea1.1/obj/Debug/net7.0/tarea1.1.AssemblyInfo.cs
+++ Lenguaje_ProgramacionI/Laboratorio/tarea1.1/obj/Debug/net7.0/tarea1.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tarea1.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afefece56cfdb76665f3e86da952e356f533045a")]
 [assembly: System.Reflection.AssemblyProductAttribute("tarea1.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tarea1.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lenguaje_ProgramacionII/JP_1_1.java
@@ -0,0 +1,17 @@
+package VarTest;
+
+public class VarTest{
+
+    public static void main(String[] args){
+        int iNum = 25;
+        char cVal = 'B';
+        boolean bVal = true;
+
+        //Mostrando en pantalla valores
+
+        System.out.println(iNum);
+        System.out.println(cVal);
+        System.out.println(bVal);
+
+    }
+}
