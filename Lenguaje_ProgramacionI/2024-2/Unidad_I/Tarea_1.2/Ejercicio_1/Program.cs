9991bdb76877abc51e07944d6d127f8be915acce
williams <williamsgamer02@gmail.com>
2024-09-02 14:43:49 -0400

    Tarea 1.2

---
+++ Lenguaje_ProgramacionI/2024-2/Unidad_I/Tarea_1.2/Ejercicio_1/Program.cs
@@ -0,0 +1,60 @@
+﻿class CuentaBancaria
+{
+    public string NombreTitular;
+    public string NumeroCuenta;
+    public double Saldo;
+
+    public CuentaBancaria(string nombreTitular, string numeroCuenta, double saldo)
+    {
+        NombreTitular = nombreTitular;
+        NumeroCuenta = numeroCuenta;
+        Saldo = saldo;
+    }
+
+    public void Depositar(double cantidad)
+    {
+        Saldo += cantidad;
+    }
+
+    public void Retirar(double cantidad)
+    {
+        Saldo -= cantidad;
+    }
+
+    public void MostrarDetalles()
+    {
+        Console.WriteLine($"Nombre Titular: {NombreTitular}");
+        Console.WriteLine($"Número de Cuenta: {NumeroCuenta}");
+        Console.WriteLine($"Saldo: ${Saldo:F2}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CuentaBancaria cuenta1 = new CuentaBancaria("Juan Perez", "1234567890", 1000.0);
+        CuentaBancaria cuenta2 = new CuentaBancaria("Maria Ramirez", "1053367841", 1500.0);
+
+        Console.WriteLine("Detalles de la Cuenta 1:");
+        cuenta1.MostrarDetalles();
+        Console.WriteLine();
+
+        Console.WriteLine("Detalles de la Cuenta 2:");
+        cuenta2.MostrarDetalles();
+        Console.WriteLine();
+
+        cuenta1.Depositar(2000);
+        cuenta2.Retirar(500);
+
+        Console.WriteLine("Detalles de la Cuenta 1 despues del deposito: ");
+        cuenta1.MostrarDetalles();
+        Console.WriteLine();
+
+        Console.WriteLine("Detalles de la Cuenta 2 despues del retiro: ");
+        cuenta2.MostrarDetalles();
+        Console.WriteLine();
+
+        Console.ReadLine();
+    }
+}
---
+++ Lenguaje_ProgramacionI/2024-2/Unidad_I/Tarea_1.2/Ejercicio_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BasicOOPExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce tu nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Introduce tu edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            Person person = new Person(nombre, edad);
+
+            Console.WriteLine("Hola " + person.nombre + ", tienes " + person.edad + " años.");
+
+            Console.ReadKey();
+        }
+        
+    }
+
+    class Person
+    {
+        public string nombre;
+        public int edad;
+
+        public Person(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+    }
+}
